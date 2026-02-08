e1e26450b67ea9d8b0a341dcbeee7b1621a76f39
shantokhn9988-rgb <shantokhn9988@gmail.com>
2026-02-08 11:51:30 +0600

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("io.coil-kt:coil-compose:2.6.0") // Images
+    implementation("androidx.media3:media3-exoplayer:1.3.0") // Video Player
+    implementation("androidx.media3:media3-ui:1.3.0")
+}
